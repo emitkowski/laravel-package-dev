fe85ac4cbfa6077651eceeb56b25cd27aa977fb0
Eric Mitkowski <eric.mitkowski@rightstart.com>
2015-05-13 16:51:12 -0600

    remove packages dir and gitignore

--- packages/larablocks/pigeon/src/Pigeon/InvalidMessageTypeException.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Larablocks\Pigeon;
-
-use Exception;
-
-class InvalidMessageTypeException extends Exception
-{
-}
--- packages/larablocks/pigeon/src/Pigeon/UnknownMessageTypeException.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Larablocks\Pigeon;
-
-use Exception;
-
-class UnknownMessageTypeException extends Exception
-{
-}
